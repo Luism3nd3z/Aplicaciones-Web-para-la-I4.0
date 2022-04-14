b85f1ffbc6fd19d1c7a6a8addfb0e1a38fe93e70
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-14 03:03:56 +0000

    user_view admin

---
+++ mvc/controllers/admin/user_view.py
@@ -0,0 +1,19 @@
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
+
+app = web.application(urls, globals()) 
+render = web.template.render('mvc/controllers/admin/', base="layout")
+
+
+class Userview:                             
+    def GET(self,localId):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig) 
+            db = firebase.database() 
+            user =  db.child("usuarios").child(localId).get()
+            return render.user_view(user) 
+        except Exception as error: 
+            message = "Error en el sistema"
+            print("Error Login.GET: {}".format(error))
