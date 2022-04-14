85bb2a9781f554713216ee11239406aa87679488
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-14 03:07:25 +0000

    user_list admin

---
+++ mvc/controllers/admin/user_list.py
@@ -0,0 +1,17 @@
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
+
+app = web.application(urls, globals()) 
+render = web.template.render('mvc/controllers/admin/', base="layout")
+
+class UsersList:                             
+    def GET(self):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig) 
+            db = firebase.database() 
+            users = db.child("usuarios").get()
+            return render.users_list(users) 
+        except Exception as error: 
+            print("Error Login.GET: {}".format(error))
