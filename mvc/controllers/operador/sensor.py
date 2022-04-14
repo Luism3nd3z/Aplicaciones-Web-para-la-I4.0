7e607fba8788dc136ba376cb0912769d0db3a8c0
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-14 06:04:38 +0000

    sensor

---
+++ mvc/controllers/operador/sensor.py
@@ -0,0 +1,17 @@
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
+
+app = web.application(urls, globals()) 
+render = web.template.render('mvc/controllers/operador/', base="layout")
+
+
+class Sensor:
+      def GET(self):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig) 
+            db = firebase.database() 
+            return render.sensor() 
+        except Exception as error: 
+            print("Error sensor.GET: {}".format(error))
