8f6c9927c6af0cbddeb47df03ae9916269d958f4
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-14 02:54:04 +0000

    bienvenida_administrador

---
+++ mvc/controllers/admin/bienvenida_administrador.py
@@ -0,0 +1,15 @@
+import web # libreria para usar el framework web.py
+import pyrebase # libreria para contecarse con firebase
+import firebase_config as token # archivo con la configuracion de firebase
+import json # libreria para manejar el formato JSON
+
+app = web.application(urls, globals()) 
+render = web.template.render('mvc/controllers/admin/', base="layout")
+
+
+class Bienvenida_administrador:
+    def GET(self): 
+        if ( web.cookies().get('localid')) != "": # cookie
+            return render.bienvenida_administrador() # redirecinar a bienevenida
+        else:
+            return render.login() # redirecinar a login
