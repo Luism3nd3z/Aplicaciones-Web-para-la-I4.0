ecd3cc22db6a38a283d6054b22f1cb07044d9c75
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-14 06:01:08 +0000

    bienvenida_usuario

---
+++ mvc/controllers/operador/bienvenida_usuario.py
@@ -0,0 +1,15 @@
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
+
+app = web.application(urls, globals()) 
+render = web.template.render('mvc/controllers/operador/', base="layout")
+
+
+class Bienvenida_usuario:
+    def GET(self): 
+        if ( web.cookies().get('localid')) != "": # cookie
+            return render.bienvenida_usuario() # redirecinar a bienevenida
+        else:
+            return render.login() # redirecinar a login
