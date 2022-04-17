323965a29614a93aedb19ae95901fa66617ca1ee
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-17 16:56:08 +0000

    admin bienvenida

--- mvc/controllers/public/inicio.py
+++ mvc/controllers/public/inicio.py
@@ -1,5 +1,7 @@
-import web
-import app
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
 
 render = web.template.render('mvc/controllers/public/', base="layout")
 
